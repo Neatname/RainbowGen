444426835d13f9ccb00b0b4b9c15b45a74a73028
Nathan Miles <nathan.miles95@gmail.com>
2016-07-17 18:34:15 -0400

    Massive performance increase when image contains fewer than 128^3 pixels.

--- src/main/java/com/nmiles/rainbowgen/generator/ColorTracker.java
+++ src/main/java/com/nmiles/rainbowgen/generator/ColorTracker.java
@@ -10,25 +10,38 @@ class ColorTracker {
 	
 	private int MAX_DIMENSION = 256;
 	
+	private int dimension;
+	
+	private int lastCell;
+	
+	private int scalar;
+	
 	public static final int MAX_COLORS = 16777216;
 	Random rand;
 
 	ColorTracker(int colors) {
 		rand = new Random();
-		fillRandomColors(colors);
+		fill(colors);
+		lastCell = dimension - 1;
 	}
 	
-	private void fillRandomColors(int colors){
-        tracker = new int[256][256][256];
-        for (int i = 0; i < colors; i++){
-            int r = rand.nextInt(256);
-            int g = rand.nextInt(256);
-            int b = rand.nextInt(256);
-            if (tracker[r][g][b] == 0){
-            	tracker[r][g][b] = new Color(r, g, b, 255).getRGB();
-            } else {
-                i--;
-            }
+	private void fill(int colors){
+		dimension = 1;
+		while (dimension * dimension * dimension < colors){
+			dimension *= 2;
+		}
+		scalar = MAX_DIMENSION / dimension;
+        tracker = new int[dimension][dimension][dimension];
+        int rToSet, gToSet, bToSet;
+        for (int r = 0; r < dimension; r++){
+        	rToSet = r * scalar;
+        	for (int g = 0; g < dimension; g++){
+        		gToSet = g * scalar;
+        		for (int b = 0; b < dimension; b++){
+        			bToSet = b * scalar;
+        			tracker[r][g][b] = (new Color(rToSet, gToSet, bToSet)).getRGB();
+        		}
+        	}
         }
     }
 	
@@ -37,9 +50,10 @@ class ColorTracker {
         int rToCheck;
         int gToCheck;
         int bToCheck;
-        int currentR = (colorToMatch >> 16) & 0xFF;
-        int currentG = (colorToMatch >> 8) & 0xFF;
-        int currentB = colorToMatch & 0xFF;
+        Color c = new Color(colorToMatch);
+        int currentR = c.getRed() / scalar;
+        int currentG = c.getGreen() / scalar;
+        int currentB = c.getBlue() / scalar;
         int range = 0;
         int redStart, redEnd,
         	greenStart, greenEnd,
@@ -49,9 +63,9 @@ class ColorTracker {
             rToCheck = currentR - range;
             if (rToCheck >= 0){
             	greenStart = Math.max(0, currentG - range);
-            	greenEnd = Math.min(255, currentG + range);
+            	greenEnd = Math.min(lastCell, currentG + range);
             	blueStart = Math.max(0, currentB - range);
-            	blueEnd = Math.min(255, currentB + range);
+            	blueEnd = Math.min(lastCell, currentB + range);
                 for (gToCheck = greenStart; gToCheck <= greenEnd; gToCheck++){
                     for (bToCheck = blueStart; bToCheck <= blueEnd; bToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -61,11 +75,11 @@ class ColorTracker {
                 }
             }
             rToCheck = currentR + range;
-            if (rToCheck < 256){
+            if (rToCheck < dimension){
             	greenStart = Math.max(0, currentG - range);
-            	greenEnd = Math.min(255, currentG + range);
+            	greenEnd = Math.min(lastCell, currentG + range);
             	blueStart = Math.max(0, currentB - range);
-            	blueEnd = Math.min(255, currentB + range);
+            	blueEnd = Math.min(lastCell, currentB + range);
                 for (gToCheck = greenStart; gToCheck <= greenEnd; gToCheck++){
                     for (bToCheck = blueStart; bToCheck <= blueEnd; bToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -77,9 +91,9 @@ class ColorTracker {
             gToCheck = currentG - range;
             if(gToCheck >= 0){
             	redStart = Math.max(0, currentR - range + 1);
-            	redEnd = Math.min(255, currentR + range - 1);
+            	redEnd = Math.min(lastCell, currentR + range - 1);
             	blueStart = Math.max(0, currentB - range);
-            	blueEnd = Math.min(255, currentB + range);
+            	blueEnd = Math.min(lastCell, currentB + range);
                 for (rToCheck = redStart; rToCheck <= redEnd; rToCheck++){
                     for (bToCheck = blueStart; bToCheck <= blueEnd; bToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -89,11 +103,11 @@ class ColorTracker {
                 }
             }
             gToCheck = currentG + range;
-            if(gToCheck < 256){
+            if(gToCheck < dimension){
             	redStart = Math.max(0, currentR - range + 1);
-            	redEnd = Math.min(255, currentR + range - 1);
+            	redEnd = Math.min(lastCell, currentR + range - 1);
             	blueStart = Math.max(0, currentB - range);
-            	blueEnd = Math.min(255, currentB + range);
+            	blueEnd = Math.min(lastCell, currentB + range);
                 for (rToCheck = redStart; rToCheck <= redEnd; rToCheck++){
                     for (bToCheck = blueStart; bToCheck <= blueEnd; bToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -105,9 +119,9 @@ class ColorTracker {
             bToCheck = currentB - range;
             if (bToCheck >= 0){
             	redStart = Math.max(0, currentR - range + 1);
-            	redEnd = Math.min(255, currentR + range - 1);
+            	redEnd = Math.min(lastCell, currentR + range - 1);
             	greenStart = Math.max(0, currentG - range + 1);
-            	greenEnd = Math.min(255, currentG + range - 1);
+            	greenEnd = Math.min(lastCell, currentG + range - 1);
                 for (rToCheck = redStart; rToCheck <= redEnd; rToCheck++){
                     for (gToCheck = greenStart; gToCheck <= greenEnd; gToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -117,11 +131,11 @@ class ColorTracker {
                 }
             }
             bToCheck = currentB + range;
-            if (bToCheck < 256){
+            if (bToCheck < dimension){
             	redStart = Math.max(0, currentR - range + 1);
-            	redEnd = Math.min(255, currentR + range - 1);
+            	redEnd = Math.min(lastCell, currentR + range - 1);
             	greenStart = Math.max(0, currentG - range + 1);
-            	greenEnd = Math.min(255, currentG + range - 1);
+            	greenEnd = Math.min(lastCell, currentG + range - 1);
                 for (rToCheck = redStart; rToCheck <= redEnd; rToCheck++){
                     for (gToCheck = greenStart; gToCheck <= greenEnd; gToCheck++){
                         if (tracker[rToCheck][gToCheck][bToCheck] != 0){
@@ -137,18 +151,19 @@ class ColorTracker {
 	
 	
 	void markUsed(int color){
-		int r = (int)((color >> 16) & 0xFF);
-		int g = (int)((color >> 8) & 0xFF);
-		int b = (int)(color & 0xFF);
+		Color c = new Color(color);
+		int r = c.getRed() / scalar;
+		int g = c.getGreen() / scalar;
+		int b = c.getBlue() / scalar;
 		tracker[r][g][b] = 0;
 	}
 	
 	int getRandomUnused(){
 		int ret = 1;
 		while (ret == 1){
-			int r = rand.nextInt(tracker.length);
-			int g = rand.nextInt(tracker.length);
-			int b = rand.nextInt(tracker.length);
+			int r = rand.nextInt(dimension);
+			int g = rand.nextInt(dimension);
+			int b = rand.nextInt(dimension);
 			if (tracker[r][g][b] != 0){
 				ret = tracker[r][g][b];
 			}
